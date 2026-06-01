42a3877ffc98ed4535eaea4df782dbf7c046b382
Junaidkhatri98 <work@teque7.com>
2026-06-01 20:26:05 +0530

    Update Pixel code

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -19,16 +19,16 @@ export default function Analytics() {
       {/* Meta Pixel Code */}
       <Script id="fb-pixel" strategy="afterInteractive">
         {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '382106638196724');
-          fbq('track', 'PageView');
+         !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2727992664229748');
+fbq('track', 'PageView');
         `}
       </Script>
       <noscript>
@@ -37,7 +37,7 @@ export default function Analytics() {
           height="1"
           width="1"
           style={{ display: "none" }}
-          src="https://www.facebook.com/tr?id=382106638196724&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=2727992664229748&ev=PageView&noscript=1"
           alt=""
         />
       </noscript>
